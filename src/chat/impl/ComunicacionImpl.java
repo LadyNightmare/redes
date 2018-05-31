51c29c62af6c9728f4214b11d94c2ae03e1d2a53
LadyNightmare <ladynightmare2@outlook.com>
2018-05-31 11:31:21 +0200

    Some methods completed

--- src/chat/impl/ComunicacionImpl.java
+++ src/chat/impl/ComunicacionImpl.java
@@ -1,7 +1,9 @@
 package chat.impl;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
 
 import chat.ifaces.Comunicacion;
 import chat.ifaces.Controlador;
@@ -9,29 +11,76 @@ import chat.impl.DialogoPuerto.PuertoAlias;
 
 // Clase a implementar 
 public class ComunicacionImpl implements Comunicacion {
+	
+	Controlador c;
+	MulticastSocket multiSock;
+	String userAlias;
 
 	@Override
 	public void crearSocket(PuertoAlias pa) {
+		
+		try {
+			
+			multiSock = new MulticastSocket(pa.puerto);
+			this.userAlias = pa.alias;
+			
+		} catch (IOException e) {
+			
+			System.out.println(e.getMessage());
+			
+		}
+		
 	}
 
 	@Override
 	public void setControlador(Controlador c) {
+		
+		this.c = c;
+		
 	}
 
 	@Override
 	public void runReceptor() {
+		
+		
+		
 	}
 
 	@Override
 	public void envia(InetSocketAddress sa, String mensaje) {
+		
+		
+		
 	}
 
 	@Override
 	public void joinGroup(InetAddress multi) {
+		
+		try {
+			
+			multiSock.joinGroup(multi);
+			
+		} catch (IOException e) {
+			
+			System.out.println(e.getMessage());
+			
+		}
+		
 	}
 
 	@Override
 	public void leaveGroup(InetAddress multi) {
+		
+		try {
+			
+			multiSock.leaveGroup(multi);
+			
+		} catch (IOException e) {
+			
+			System.out.println(e.getMessage());
+			
+		}
+		
 	}
 
 }
