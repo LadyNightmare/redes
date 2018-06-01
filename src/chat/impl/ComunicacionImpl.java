a178d6fa674d2256320819ed0151bf7935b43215
LadyNightmare <ladynightmare2@outlook.com>
2018-06-01 10:45:05 +0200

    I guess it's solved but got to test it

--- src/chat/impl/ComunicacionImpl.java
+++ src/chat/impl/ComunicacionImpl.java
@@ -1,9 +1,18 @@
 package chat.impl;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
 
 import chat.ifaces.Comunicacion;
 import chat.ifaces.Controlador;
@@ -15,6 +24,8 @@ public class ComunicacionImpl implements Comunicacion {
 	Controlador c;
 	MulticastSocket multiSock;
 	String userAlias;
+	DatagramPacket p = new DatagramPacket(new byte[256], 256);
+	
 
 	@Override
 	public void crearSocket(PuertoAlias pa) {
@@ -42,6 +53,25 @@ public class ComunicacionImpl implements Comunicacion {
 	@Override
 	public void runReceptor() {
 		
+		DatagramSocket dataSock;		
+		DatagramPacket data = new DatagramPacket(new byte[256], 256);
+		
+		try {
+			
+			dataSock = new DatagramSocket();
+			multiSock.send(data);
+			
+		} catch (SocketException e) {
+			
+			System.out.println(e.getMessage());
+			
+		} catch (IOException e) {
+			
+			System.out.println(e.getMessage());
+			
+		}
+		
+		
 		
 		
 	}
@@ -49,7 +79,31 @@ public class ComunicacionImpl implements Comunicacion {
 	@Override
 	public void envia(InetSocketAddress sa, String mensaje) {
 		
+		DatagramSocket dataSock;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Write your message.\n");
+		
+		byte[] word = sc.nextLine().getBytes();
+		
+		DatagramPacket data = new DatagramPacket(word, word.length, multiSock.getInetAddress(), multiSock.getLocalPort());
+		
+		try {
+			
+			dataSock = new DatagramSocket();
+			multiSock.receive(data);
+			dataSock.close();
+			
+		} catch (SocketException e) {
+			
+			System.out.println(e.getMessage());
+			
+		} catch (IOException e) {
+			
+			System.out.println(e.getMessage());
+			
+		}
 		
 	}
 
